c2f16e1bc910d7de7465a271796e7e70bcbd12e9
ryu <developpeurcurieux@gmail.com>
2020-02-07 07:59:54 +0100

    1.0.0 - init project MyBoutique

---
+++ app/src/main/java/fr/easysoft/myboutique/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.easysoft.myboutique
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
